4bcaf7204f9584676c85f3d613d0e140f212f1e7
Siriyakorn <sipa@kkumail.com>
2016-11-12 16:17:53 +0700

    Alert Choose

--- app/src/main/java/kku/friend/easykku/SignUpActivity.java
+++ app/src/main/java/kku/friend/easykku/SignUpActivity.java
@@ -1,6 +1,11 @@
 package kku.friend.easykku;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,7 +21,11 @@ public class SignUpActivity extends AppCompatActivity {
             userEditText, passwordEditText;
     private ImageView imageView;
     private Button button;
-    private String nameString, phoneString, userString, passwordString;
+    private String nameString, phoneString, userString, passwordString,
+            imagePathString, imageNameString;
+    private Uri uri;
+    private boolean aBoolean = true;
+
 
 
     @Override
@@ -47,12 +56,20 @@ public class SignUpActivity extends AppCompatActivity {
 
                 // Check Space
                 if (nameString.equals("") || phoneString.equals("") ||
-                        userString.equals("") || passwordString.equals("")){
+                        userString.equals("") || passwordString.equals("")) {
                     //Have Space
                     Log.d("12novV1", "Have Space");
                     MyAlert myAlert = new MyAlert(SignUpActivity.this, R.drawable.doremon48,
                             "มีช่องว่าง", "กรุณากรอกให้ครบทุกช่องค่ะ");
                     myAlert.myDialog();
+                } else if (aBoolean) {
+                    //Non Choose Image
+                    MyAlert myAlert = new MyAlert(SignUpActivity.this, R.drawable.nobita48, "ยังไม่มีรูป",
+                            "กรุณาเลือกรูปด้วยค่ะ");
+                    myAlert.myDialog();
+                } else {
+                    //Choose Image OK
+                    upLoadImageToServer();
                 }
 
             } //onClick
@@ -72,17 +89,65 @@ public class SignUpActivity extends AppCompatActivity {
 
     } // Main Method
 
+    private void upLoadImageToServer() {
+
+    }
+
     @Override
     protected void onActivityResult(int requestCode,
                                     int resultCode,
                                     Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if ((requestCode == 0) && (requestCode == RESULT_OK)) {
+
+
+        if ((requestCode == 0) && (resultCode == RESULT_OK)) {
+
 
             Log.d("12novV1", "Result OK");
+            aBoolean = false;
+
+            //Show Image
+            uri = data.getData();
+            try {
+
+                Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
+                imageView.setImageBitmap(bitmap);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            //Find Path of Image
+            imagePathString = myFindPath(uri);
+            Log.d("12novV1", "imagePath ==>" + imagePathString);
+
+            //Find Name of Image
+            imageNameString = imagePathString.substring(imagePathString.lastIndexOf("/"));
+            Log.d("12novV1", "imageName ==>" + imageNameString);
+
+
 
         } // if
 
     } // onActivity
+
+    private String myFindPath(Uri uri) {
+
+        String result = null;
+        String[] strings = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(uri, strings, null, null, null);
+
+        if (cursor != null) {
+
+            cursor.moveToFirst();
+            int index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            result = cursor.getString(index);
+
+        } else {
+            result = uri.getPath();
+        }
+
+        return result;
+    }
 } // Main Class
